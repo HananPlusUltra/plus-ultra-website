eaec3ea3072468ff37a7b0eefffd69fb2da6d014
slaven <slavendjervida@gmail.com>
2023-06-14 18:29:38 +0200

    add OG img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
+import Head from 'next/head';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -17,6 +18,14 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
+			<Head>
+				<meta
+					property='og:image'
+					content='https://scontent-vie1-1.cdninstagram.com/v/t51.2885-15/275744187_1124839878264366_5327447242546037122_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_cat=106&_nc_ohc=snTzyTKTcwkAX9ryIzb&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=Mjc5MTgzNzIxOTg3MjI3OTUwNA%3D%3D.2-ccb7-5&oh=00_AfAk7T2wTQGp7m048gh5EbXt9VSbPUYyFg2a2mUWRF3rBQ&oe=648EC0BE&_nc_sid=a1ad6c'
+				/>
+				<meta property='og:image:width' content='1200' />
+				<meta property='og:image:height' content='630' />
+			</Head>
 			<body
 				className={
 					inter.className +
